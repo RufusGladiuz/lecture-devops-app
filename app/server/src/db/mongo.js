6706de2b95369757ab2db12041010065f339dff2
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-06-27 11:34:37 +0200

    Added path again

--- app/server/src/db/mongo.js
+++ app/server/src/db/mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+var path = require('path');
 require('dotenv').config();
+require('dotenv').config({ path: path.join(process.env.WORKSPACE, "app", "server", "dev.env" ) });
 console.log(process.env);
 console.log("MongoDB Url ", process.env.MONGODB_URL);
-
 
 const mongooseInstance_ = mongoose.connect(
     String(process.env.MONGODB_URL),
