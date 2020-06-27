2d2a4e3ce083de57c14a2c6a92cb7e6f9f725b57
Onur Özkan <onur-ozkan@hotmail.de>
2020-06-27 22:28:21 +0200

    changes

--- app/server/src/db/mongo.js
+++ app/server/src/db/mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 var path = require('path');
-// require('dotenv').config();
-// require('dotenv').config({ path: path.join(process.env.WORKSPACE, "app", "server", "dev.env" ) });
-// console.log("MongoDB Url ", process.env.MONGODB_URL);
-// console.log(process.env);
+require('dotenv').config();
+require('dotenv').config({ path: path.join(process.env.WORKSPACE, "app", "server", "dev.env" ) });
+console.log("MongoDB Url ", process.env.MONGODB_URL);
+console.log(process.env);
 
 const mongooseInstance_ = mongoose.connect(
     process.env.MONGODB_URL,
