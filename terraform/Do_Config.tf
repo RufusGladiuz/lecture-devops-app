86cf046dc14b99ba4f213c3083fb6e78605527c8
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-07-09 20:15:53 +0200

    update terraformfile

--- terraform/Do_Config.tf
+++ terraform/Do_Config.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "web1" {
   image = "ubuntu-18-04-x64"
   name = "DevOpsSoSe2012"
   region = "FRA1"
-  size = "512mb"
+  size = "2GB"
   private_networking = true
   ssh_keys = [
     var.ssh_fingerprint
@@ -27,7 +27,22 @@ provisioner "remote-exec" {
         "sudo apt-get update",
         "sudo apt install git -y",
 
+        "sudo apt update",
+        "sudo apt install nano -y",
 
+        "sudo apt update",
+        "sudo apt install openjdk-8-jdk -y",
+
+        //Install Jenkins
+        "wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -",
+        "sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+        "sudo apt update",
+        "sudo apt install jenkins -y",
+        "sudo systemctl start jenkins",
+
+        //TODO: ADD Docker
+        //TODO: ADD Docker Compose
+        //TODO: Impliment monitor software of choice
     ]
 }
 
