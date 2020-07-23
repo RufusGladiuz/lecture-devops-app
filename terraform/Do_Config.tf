400c459740b72e87246cb634d48ff08a489abaa4
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-07-23 18:55:31 +0200

    [terraform/Do_Config] Added download of jenkins cli and creating a default user

--- terraform/Do_Config.tf
+++ terraform/Do_Config.tf
@@ -18,9 +18,12 @@ resource "digitalocean_droplet" "web1" {
 
 provisioner "remote-exec" {
     inline =[
+
+      //Install basics
         "sudo apt-get update",
         "sudo apt install npm -y",
 
+      //TODO: Install nodeJS > 10.0
         "sudo apt-get update",
         "sudo apt install nodejs -y",
 
@@ -41,19 +44,28 @@ provisioner "remote-exec" {
         "sudo systemctl start jenkins",
 
         //Setup jenkins pipeline
-        "ip route get 1.2.3.4 | awk '{print $7}'",
-        "sudo wget http://$(!!):8080/jnlpJars/jenkins-cli.jar",
+        //"ip route get 1.2.3.4 | awk '{print $7}'",
+        "sudo sleep 30",
+        "sudo wget http://`ip route get 1.2.3.4 | awk '{print $7}'`:8080/jnlpJars/jenkins-cli.jar",
+        "sudo echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount(\"devops\", \"admin123\")' | java -jar jenkins-cli.jar -auth admin:`cat /var/lib/jenkins/secrets/initialAdminPassword` -s http://localhost:8080/ groovy ="
 
+        //"curl -L https://github.com/jenkins-zh/jenkins-cli/releases/latest/download/jcli-linux-amd64.tar.gz|tar xzv",
+        //"sudo mv jcli /usr/local/bin/",
+        
+       //"echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount(\"temp\", \"admin123\")' | java -jar ./jenkins-cli.jar -s \"http://localhost:8080\" -auth admin:`/var/lib/jenkins/secrets/initialAdminPassword` -noKeyAuth groovy = –",
         //Set up python and relevant packages
-        "sudo apt update",
-        "sudo apt install python3.8 -y",
-        "sudo apt update",
-        "apt install python3-pip -y",
-        "python3 -m pip install jenkinsapi",
+        //"sudo apt update",
+        //"sudo apt install python3.8 -y",
+        //"sudo apt update",
+        //"apt install python3-pip -y",
+        //"python3 -m pip install jenkinsapi",
 
         //TODO: ADD Docker
         //TODO: ADD Docker Compose
         //TODO: Impliment monitor software of choice
+
+        //TODO: Hand docker user rights to jenkins
+        //TODO: Restart jenkins
 
 
 
